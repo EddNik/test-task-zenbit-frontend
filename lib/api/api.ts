d0f8b65caef14da90b8e5b4ade4b01cd3549cedc
Eduard <nikinna86@gmail.com>
2026-02-21 00:34:11 +0200

    update

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -4,7 +4,8 @@ const API_TOKEN = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({
   // baseURL: API_TOKEN + "/api",
-  baseURL: API_TOKEN || "",
+  // baseURL: API_TOKEN || "",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
